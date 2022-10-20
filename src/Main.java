be8ef88b38c12929d7b83b7ed1482a0309b86801
Anton Fougner <fougneranton@gmail.com>
2022-10-20 11:09:28 +0200

    Allt klart

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,6 @@ public class Main {
         fileHandler.printToFileHandler(person, program.outFilePath);
         program.systemOutHandler(person);
 
-        /*
-        Person kund = program.checkListAgainstUserInput(program.inputHandler("",false));
-
-        System.out.println(kund.name);
-        System.out.println(kund.idNumber);
-        System.out.println(kund.lastPaymentDate);
-        System.out.println("has paid: " + kund.hasPaid);
-        System.out.println("in system: " + kund.isInProgram());
-        */
     }
     public static void main(String[] args) {
         new Main();
